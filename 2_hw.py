f0376ff2563e54bbc5c610500f276c403a1213ad
M-Doronin <doroninn@mail.ru>
2026-04-10 15:12:59 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,14 @@
+def task_1() -> None:
+    my_int: int = 42
+    my_float: float = 3.14
+    my_str: str = "Привет, мир!"
+    my_list: list = [1, 2, 3, "элемент", True]
+    my_bool: bool = True
 
+    print(f"Тип переменной my_int: {type(my_int)}")
+    print(f"Тип переменной my_float: {type(my_float)}")
+    print(f"Тип переменной my_str: {type(my_str)}")
+    print(f"Тип переменной my_list: {type(my_list)}")
+    print(f"Тип переменной my_bool: {type(my_bool)}")
+
+task_1()
