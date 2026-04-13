5018ec47088463fe4d4f9cae8c22f4aefb14b103
M-Doronin <doroninn@mail.ru>
2026-04-13 21:18:25 +0300

    Update car.py

--- car.py
+++ car.py
@@ -1,1 +1,29 @@
+class Car:
+    def __init__(self, color, car_type, year):
 
+        self.color = color
+        self.type = car_type
+        self.year = year
+
+    def start_engine(self):
+
+        print("Автомобиль заведен")
+
+    def stop_engine(self):
+
+        print("Автомобиль заглушен")
+
+    def set_year(self, year):
+
+        self.year = year
+        print(f"Год выпуска автомобиля изменен на {year}")
+
+    def set_type(self, car_type):
+
+        self.type = car_type
+        print(f"Тип автомобиля изменен на '{car_type}'")
+
+    def set_color(self, color):
+
+        self.color = color
+        print(f"Цвет автомобиля изменен на '{color}'")
