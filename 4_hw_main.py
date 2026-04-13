8ab9ad7e99695ca492cb841b879303261ab93c0d
M-Doronin <doroninn@mail.ru>
2026-04-13 21:19:33 +0300

    Update 4_hw_main.py

--- 4_hw_main.py
+++ 4_hw_main.py
@@ -1,1 +1,22 @@
+from car import Car
 
+
+my_car = Car("красный", "седан", 2020)
+
+
+print(f"Начальные характеристики: цвет — {my_car.color}, тип — {my_car.type}, год — {my_car.year}")
+print()
+
+
+my_car.start_engine()
+my_car.stop_engine()
+print()
+
+
+my_car.set_year(2023)
+my_car.set_type("внедорожник")
+my_car.set_color("синий")
+print()
+
+
+print(f"Обновлённые характеристики: цвет — {my_car.color}, тип — {my_car.type}, год — {my_car.year}")
